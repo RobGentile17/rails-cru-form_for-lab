a4fb8e8af62ee30b38deed2cf8a992640b92271e
Rob <robert.gentile@powerhrg.com>
2021-09-23 08:03:27 -0400

    completed assignment

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,36 @@
 class GenresController < ApplicationController
+
+  def index
+    @genres = Genres.all 
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to genre_path(@genre)
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit!
+  end
 end
